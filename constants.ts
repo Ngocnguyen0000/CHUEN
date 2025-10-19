dc30c5dea76885387ddff48c7bf610359996db7c
Bich Ngoc Dev <2ndevdes@gmail.com>
2025-10-20 00:44:24 +0700

    1

--- constants.ts
+++ constants.ts
@@ -272,7 +272,7 @@ export const allProjects: Project[] = [
         id: 304,
         title: 'Mobie App | Runzy',
         category: ProjectCategory.UX_UI_DESIGN,
-        imageUrl: '/asset/uxui_design/304.png',
+        imageUrl: '/asset/uxui_design/304.PNG',
         projectUrl:
             'https://www.behance.net/gallery/236869453/Mobie-App-Runzy',
         gallery: ['/asset/uxui_design/304.1.png'],
